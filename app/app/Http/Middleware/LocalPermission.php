250c7024cf562d672324d7b047ecdd7f151e7fd3
Thinh Nguyen <misddev@gmail.com>
2020-11-13 17:02:31 +0700

    Runner

--- app/app/Http/Middleware/LocalPermission.php
+++ app/app/Http/Middleware/LocalPermission.php
@@ -18,6 +18,7 @@ class LocalPermission
      */
     public function handle($request, Closure $next)
     {
+        dd($request->header('local_token'));
         $token = $request->header('local_token') ?? null;
         dd($token);
         if ($token == 'dC38Xaq0YO03A3fRbaitx78zmYhvIPSjsOjVB4VGpfOtNLar37gPphE1tlfJIZxs') {
