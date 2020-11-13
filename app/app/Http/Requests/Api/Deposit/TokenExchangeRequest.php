c59411bfa06646b67e989092dc26fedb43f03871
Thinh Nguyen <misddev@gmail.com>
2020-11-13 17:07:55 +0700

    Runner

--- app/app/Http/Requests/Api/Deposit/TokenExchangeRequest.php
+++ app/app/Http/Requests/Api/Deposit/TokenExchangeRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Validation\Rule;
 
-class PublicApiRequest extends FormRequest
+class TokenExchangeRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
